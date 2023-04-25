9442fe0d509f2f6fe4ffb09838ccc8a302e7c162
“chapaofan-zy” <1095004630@qq.com>
2023-04-25 10:00:43 +0800

    feat: 接入mongoDB云

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
-const DBRootModule = MongooseModule.forRoot('mongodb://127.0.0.1/test');
+// const DBRootModule = MongooseModule.forRoot('mongodb://127.0.0.1/test');
+const DBRootModule = MongooseModule.forRoot(
+  'mongodb+srv://1095004630:zhangY18@chapaofan.kfljrnd.mongodb.net/test',
+);
 
 @Module({
   imports: [UserModule, DBRootModule],
